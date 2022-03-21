eb3c270409e77bd0297a77de72703380e4f5f0df
Joseph Morkly <joseph.morkly.9@gmail.com>
2022-03-21 10:20:47 +0000

    adjustments

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,20 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate, Link, Outlet } from "react-router-dom";
 import "./Dashboard.css";
-import dash_avatar from "../../assets/github.png";
 
 const Dashboard = () => {
-  let navigate = useNavigate();
-  const [toggleState, setToggleState] = useState(1);
-
-
-  const toggleTab = (index) => {
-    setToggleState(index);
-  };
-
   return (
     <div className="dashboard-container">
-      Dashboard
+      <div>
+        <p>Dashboard (Work in progress)</p>
+      </div>
     </div>
   );
 };
